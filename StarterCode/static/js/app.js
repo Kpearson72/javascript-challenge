3f1530f7f7f717311a8570d306619ac6042e8712
Kpearson72 <kpearson72@gmail.com>
2021-02-26 20:02:54 -0600

    add references to button, date/time, city state, country, and shpae

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -39,69 +39,13 @@ function processRecord(record){
 // getting a reference to the button on the page with the id `filter-btn`
 let button = d3.select("#filter-btn");
 
-// getting a reference to the date/time input element with the class set to `form-control`
+// getting a reference to the date/time, city, state, country and shape input element with the class set to `form-control`
 let inputDate = d3.select("#datetime");
 let inputCity = d3.select("#city");
 let inputState = d3.select("#state");
 let inputCountry = d3.select("#country");
 let inputShape = d3.select("#shape");
 let clickCount = 0;
-
-// a callback-function which is triggered when the button filter table is clicked
-function handleClick(){
-    d3.event.preventDefault();
-    let inputText = inputDate.property("value");
-    let city = inputCity.property("value");
-    let state = inputState.property("value");
-    let country = inputCountry.property("value");
-    let shape = inputShape.property("value");
-    console.log(inputText);
-    console.log(city);
-    console.log(state);
-    console.log(country);
-    console.log(shape);
-    if (inputText){
-        var filterValue = tableData.filter(record => record.datetime===inputText);
-    }
-    else{
-        var filterValue = tableData
-    }
-    if (city){
-        var filterValue = filterValue.filter(record => record.city===city);
-    }
-    if (state){
-        var filterValue = filterValue.filter(record => record.state===state);
-    }
-    if (country){
-        var filterValue = filterValue.filter(record => record.country===country);
-    }
-    if (shape){
-        var filterValue = filterValue.filter(record => record.shape===shape);
-    }
-
-    console.log(filterValue);
-    tbody.html("");
-    filterValue.forEach(processRecord);
-    
-
-}
-// using on function in d3 to attach an event handler function to button
-button.on("click", handleClick)
-
-// button.on("click", function(){
-//     clickCount ++;
-//     console.log(`button click number ${clickCount}`);
-//     console.log(event.target);
-// });
-
-// // input field that changes based on a click
-// inputField.on('change', function (event){
-//     console.log(event.target);
-//     console.log(event.target.value);
-
-// })
-
-inputField.on("change", handleClick);
 
 
 
